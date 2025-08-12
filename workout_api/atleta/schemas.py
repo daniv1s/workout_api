1a4fa95d7e1a43d1905b6b618d6154fc5286095e
Daniel Lucas Leite <daniellucasleite@hotmail.com>
2025-08-11 21:12:31 -0300

    Update schemas.py

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -5,7 +5,6 @@ from workout_api.centro_treinamento.schemas import CentroTreinamentoAtleta
 
 from workout_api.contrib.schemas import BaseSchema, OutMixin
 
-
 class Atleta(BaseSchema):
     nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
     cpf: Annotated[str, Field(description='CPF do atleta', example='12345678900', max_length=11)]
@@ -24,6 +23,16 @@ class AtletaIn(Atleta):
 class AtletaOut(Atleta, OutMixin):
     pass
 
+
 class AtletaUpdate(BaseSchema):
     nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
     idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
+
+
+class AtletaListSchema(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
+    centro_treinamento: Annotated[str, Field(description='Nome do centro de treinamento')]
+    categoria: Annotated[str, Field(description='Nome da categoria')]
+
+    class Config:
+        orm_mode = True
