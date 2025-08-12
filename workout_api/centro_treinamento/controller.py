d2f574a81978a47efc8dbac904010319184056e0
Daniel Lucas Leite <daniellucasleite@hotmail.com>
2025-08-11 21:15:01 -0300

    Update controller.py

--- workout_api/centro_treinamento/controller.py
+++ workout_api/centro_treinamento/controller.py
@@ -1,11 +1,16 @@
 from uuid import uuid4
+from typing import Optional
 from fastapi import APIRouter, Body, HTTPException, status
 from pydantic import UUID4
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.future import select
+
+from fastapi_pagination import Page, paginate
+
 from workout_api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut
 from workout_api.centro_treinamento.models import CentroTreinamentoModel
 
 from workout_api.contrib.dependencies import DatabaseDependency
-from sqlalchemy.future import select
 
 router = APIRouter()
 
@@ -22,8 +27,21 @@ async def post(
     centro_treinamento_out = CentroTreinamentoOut(id=uuid4(), **centro_treinamento_in.model_dump())
     centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())
     
-    db_session.add(centro_treinamento_model)
-    await db_session.commit()
+    try:
+        db_session.add(centro_treinamento_model)
+        await db_session.commit()
+    except IntegrityError:
+        await db_session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER,
+            detail=f"Já existe um centro de treinamento cadastrado com o nome: {centro_treinamento_in.nome}"
+        )
+    except Exception:
+        await db_session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Erro ao inserir o centro de treinamento no banco"
+        )
 
     return centro_treinamento_out
     
@@ -32,14 +50,20 @@ async def post(
     '/', 
     summary='Consultar todos os centros de treinamento',
     status_code=status.HTTP_200_OK,
-    response_model=list[CentroTreinamentoOut],
+    response_model=Page[CentroTreinamentoOut],
 )
-async def query(db_session: DatabaseDependency) -> list[CentroTreinamentoOut]:
-    centros_treinamento_out: list[CentroTreinamentoOut] = (
-        await db_session.execute(select(CentroTreinamentoModel))
-    ).scalars().all()
+async def query(
+    db_session: DatabaseDependency,
+    nome: Optional[str] = None
+) -> Page[CentroTreinamentoOut]:
+    stmt = select(CentroTreinamentoModel)
+
+    if nome:
+        stmt = stmt.filter(CentroTreinamentoModel.nome.ilike(f"%{nome}%"))
+
+    centros_treinamento = (await db_session.execute(stmt)).scalars().all()
     
-    return centros_treinamento_out
+    return paginate(centros_treinamento)
 
 
 @router.get(
@@ -49,7 +73,7 @@ async def query(db_session: DatabaseDependency) -> list[CentroTreinamentoOut]:
     response_model=CentroTreinamentoOut,
 )
 async def get(id: UUID4, db_session: DatabaseDependency) -> CentroTreinamentoOut:
-    centro_treinamento_out: CentroTreinamentoOut = (
+    centro_treinamento_out = (
         await db_session.execute(select(CentroTreinamentoModel).filter_by(id=id))
     ).scalars().first()
 
